91458fc6607e3a26a0bf55a5aa5aa1197a96d728
Poplar Wang <snowlion18@gmail.com>
2021-02-16 20:51:23 -0500

    MANUAL init push

---
+++ start_server.bat
@@ -0,0 +1,25 @@
+@echo off
+title Village Valheim Server
+set SteamAppId=892970
+
+:start
+echo Starting server
+valheim_server -nographics -batchmode -name "Village" -port 2456 -world "Dedicated" -password %VALPASS%
+
+:backup
+echo Backing up files
+git add start_server.bat
+copy C:\Users\Admin\AppData\LocalLow\IronGate\Valheim\worlds\Dedicated.db C:\Users\Admin\Documents\VALHEIM_SERVER
+git add Dedicated.db
+copy C:\Users\Admin\AppData\LocalLow\IronGate\Valheim\worlds\Dedicated.fwl C:\Users\Admin\Documents\VALHEIM_SERVER
+git add Dedicated.fwl
+git commit -m "Server Save on %date% at %time%"
+git push
+
+:reboot
+@echo off
+echo Server is currently off!
+echo Press any key to restart the server.
+pause >nul
+@echo on
+goto :start
