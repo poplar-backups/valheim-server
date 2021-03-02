8304b851c3fc65ab5fcff65d2850b2f38cbcd8c6
Poplar Wang <snowlion18@gmail.com>
2021-03-02 17:11:15 -0500

    Server Save on 2021-03-02 at 17:11:15.28

--- start_server.bat
+++ start_server.bat
@@ -17,9 +17,7 @@ git commit -m "Server Save on %date% at %time%"
 git push
 
 :reboot
-@echo off
 echo Server is currently off!
 echo Press any key to restart the server.
 pause >nul
-@echo on
 goto :start
